950fc4b83ed9230430f3e73345c86d0adbd1bdfc
Sherri Chen <sherri@lnxsrv02.seas.ucla.edu>
2013-05-07 19:40:37 -0700

    for loop moved from main to individual functions

--- webapp/src/main/webapp/WebApp.java
+++ webapp/src/main/webapp/WebApp.java
@@ -6,10 +6,11 @@ import static java.lang.System.out;
 
 public class WebApp
 {
+  private static final int num_top_pages = 3; // only use top three pages
   public static void main(String args[])
   {
     String keyword = args[0]; // the keyword
-    int num_top_pages = 3; // only use top three pages
+    // int num_top_pages = 3; // only use top three pages
     WebPage[] webpages = new WebPage[num_top_pages + 1];
     int i;
     for(i = 0; i < num_top_pages+1; i++)
@@ -17,36 +18,47 @@ public class WebApp
       webpages[i] = new WebPage();
       webpages[i].set_keyword(keyword);
     }
-    //============================================= 
-    // call webservice function to get the webpages
-    //=============================================
-    webpages[0].set_content("purple is purple hippos"); // for testing
+    //================================================
+    // call webservice function to get the webpages:
+    // webservice(webpages)
+    //================================================
+    /*TESTING*/ webpages[0].set_content("purple is purple hippos"); // for testing
+    /*TESTING*/ webpages[1].set_content("purple purple more purple");
+    /*TESTING*/ webpages[2].set_content("hello candy world");
+    //for(i = 0; i < num_top_pages+1; i++)
+    //{
+      keyword_in_page(webpages);
+      keyword_in_tags(webpages);
+      keyword_in_url(webpages);
+    //}
+    /*TESTING*/ out.printf("Keys in Page0: %d\n", webpages[0].keys_in_page); // for testing
+    /*TESTING*/ out.printf("Keys in Page1: %d\n", webpages[1].keys_in_page); // for testing
+    /*TESTING*/ out.printf("Keys in Page2: %d\n", webpages[2].keys_in_page); // for testing
+
+  }
+
+  //========== Each metric has one function =================
+  public static void keyword_in_page(WebPage[] webpages)
+  {
+    int i;
     for(i = 0; i < num_top_pages+1; i++)
     {
-      keyword_in_page(webpages[i]);
-      keyword_in_tags(webpages[i]);
-      keyword_in_url(webpages[i]);
+      String pattern = webpages[i].get_keyword(); // the pattern is the keyword
+      Pattern pat = Pattern.compile(pattern); // create the pattern object
+      Matcher mat = pat.matcher(webpages[i].get_content()); // create the matcher object
+      int count = 0; // keyword count
+      while(mat.find())
+      {
+        count++;
+      }
+      webpages[i].keys_in_page = count;
     }
-    out.printf("Keys in Page: %d\n", webpages[0].keys_in_page);
   }
-  public static void keyword_in_page(WebPage webpage)
-  {
-    String pattern = webpage.get_keyword(); // the pattern is the keyword
-    Pattern pat = Pattern.compile(pattern); // create the pattern object
-    Matcher mat = pat.matcher(webpage.get_content()); // create the matcher object
-
-    int count = 0; // keyword count
-    while(mat.find())
-    {
-      count++;
-    }
-    webpage.keys_in_page = count;
-  }
-  public static void keyword_in_tags(WebPage webpage)
+  public static void keyword_in_tags(WebPage[] webpages)
   {
     ;
   }
-  public static void keyword_in_url(WebPage webpage)
+  public static void keyword_in_url(WebPage[] webpages)
   {
     ;
   }
